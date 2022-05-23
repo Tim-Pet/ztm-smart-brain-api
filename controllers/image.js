d17d4a430d404c2a4c949f501670c850113c839e
Tim-Pet <petersen@poprocket.com>
2022-05-23 14:36:00 +0200

    add env type

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 import Clarifai from 'clarifai';
 
 const app = new Clarifai.App({
-  apiKey: process.env.CLARIFAI_API_KEY,
+  apiKey: `${process.env.CLARIFAI_API_KEY}`,
 });
 
 const handleApiCall = (req, res) => {
